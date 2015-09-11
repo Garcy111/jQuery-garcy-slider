1804abd55d324af914d418b839675412ff15ae73
Garcy111 <Garcy999@yandex.ru>
2015-09-11 13:31:37 +0300

    11.09.15 13:23

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <title>Garcy Slider</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Garcy Slider</title>
     <link href="css/garcy.css" rel="stylesheet">
     <script src="http://yastatic.net/jquery/2.1.4/jquery.min.js"></script>
     <script src="/js/jquery-garcy-slider.js"></script>
